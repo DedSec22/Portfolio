2d593a90c7e5c65202feead2045ba48cef3f4175
Devdxtt <devbhoj32@gmail.com>
2024-09-17 19:10:36 +0530

    asd

--- src/jsx/Components/Socials.jsx
+++ src/jsx/Components/Socials.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Graph from '../../img/icons/Graph.svg';
 
 const Socials = (prop) => {
-    const { socialTitle, socialUsername } = prop;
+    const { socialTitle, socialUsername, URL, posts } = prop;
     return (
         <>
             <div className="socials">
                 <div className="socials-inner">
-                    <div className="container-fluid" rel="noopener noreferrer">
+                    <div className="container-fluid">
                         <div className="left-titles">
-                            <a href={`https://instagram.com/${socialUsername}`} target='_blank' className="social-title">{socialTitle}</a>
-                            <a href={`https://instagram.com/${socialUsername}`} target='_blank' className="social-username">@{socialUsername}</a>
+                            <a href={`https://instagram.com/${socialUsername}`} target='_blank' className="social-title" rel="noopener noreferrer">{socialTitle}</a>
+                            <a href={`https://instagram.com/${socialUsername}`} target='_blank' className="social-username" rel="noopener noreferrer">@{socialUsername}</a>
+                        </div>
+                        <div className="right-images">
+                            {URL.map((url, index) => (
+                                <a href={`https://instagram.com/${socialUsername}`} className='right-images-link' target='_blank' rel="noopener noreferrer">
+                                    <img key={index} src={url} alt={`Instagram post ${index + 1}`} />
+                                </a>
+                            ))}
+                        </div>
+                        <div className="total-post">
+                            <div className="total-post-inner">
+                                <span className="numbers">{posts}</span>
+                                <span className="text">posts</span>
+                            </div>
+                            <img src={Graph} alt='Graph' className='graph'/>
                         </div>
                     </div>
                 </div>
--- src/jsx/Pages/HomePage.jsx
+++ src/jsx/Pages/HomePage.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Hero from '../Components/Hero.jsx';
 import Socials from '../Components/Socials.jsx';
+import Image1 from '../../img/IMG_1.png';
+import Image2 from '../../img/IMG_2.png';
+import Image3 from '../../img/IMG_4.png';
 
 const HomePage = () => {
   const instagramUsername = ['devdxtt'];
+  const URL = [ Image2, Image3, Image1];
+  const posts = ['69'];
 
   return (
     <>
       <Hero />
-      <Socials socialTitle='Instagram' socialUsername={instagramUsername}/>
+      <Socials socialTitle='Instagram' socialUsername={instagramUsername} URL={URL} posts={posts}/>
     </>
   )
 }
